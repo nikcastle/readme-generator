2b321371961a1b44b88dfce953fa4be9c1d5eced
Lady Stock <nikki.castlea@gmail.com>
2020-07-17 16:59:34 -0600

    added call user prompts

--- index.js
+++ index.js
@@ -1,39 +1,77 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
     return inquirer
         .prompt([
             {
                 type: "input",
-                name: "name",
-                message: "What is your name?"
+                name: "title",
+                message: "What is the title of your project?"
             },
             {
                 type: "input",
-                name: "location",
-                message: "Where are you from?"
+                name: "description",
+                message: "Please write a brief description of the project."
+            },
+            // {
+            //     type: "checkbox",
+            //     name: "tableOfContents",
+            //     message: "Please check which items you would like contained in the Table of Contents.",
+            //     choices: ["Title", "Description", "Installation", "Usage", "License", "Contributing", "Author(s)", "Credit" ]
+            // },
+            {
+                type: "input",
+                name: "instal",
+                message: "Please describe what the user would need to know to install this project"
             },
             {
                 type: "input",
-                name: "hobby",
-                message: "What is your favorite hobby?"
+                name: "use",
+                message: "Please describe how to use this project. "
             },
             {
                 type: "input",
-                name: "food",
-                message: "What is your favorite food?"
+                name: "license",
+                message: "Please list any license used in this project."
             },
             {
                 type: "input",
-                name: "github",
-                message: "Enter your GitHub Username"
+                name: "contrib",
+                message: "Please describe how a user would contribute to this project."
             },
             {
                 type: "input",
-                name: "linkedin",
-                message: "Enter your LinkedIn URL."
-            }
+                name: "tests",
+                message: "Please describe any testing completed."
+            },
+            {
+                type: "input",
+                name: "questions",
+                message: "Please list questions regarding this project, if any."
+            },
 
-        ])
+        ]);
 }
+
+function generateReadMe() {
+    return `
+    `
+}
+
+promptUser ()
+    .then(function(answers){
+        const readme = generateReadMe(answers);
+
+        return writeFileAsync("README.md", readme);
+    })
+    .then(function() {
+        console.log("Successfully wrote to README.md");
+        
+    })
+    .catch(function(err) {
+        throw err;
+    })
